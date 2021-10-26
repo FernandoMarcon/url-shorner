2d6af16fd2b7a038a7a6927db00e2bf108a74008
FernandoMarcon <fmarcon13@gmail.com>
2021-10-25 23:34:03 -0300

    added error handling

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash, abort, session
 import json
 import os.path
 from werkzeug.utils import secure_filename
@@ -6,11 +6,9 @@ from werkzeug.utils import secure_filename
 app = Flask(__name__)
 app.secret_key = 'uaiusdhisudghiaubdoiayusbdouyb'
 
-
 @app.route('/')
 def home():
     return render_template('home.html', name='Name')
-
 
 @app.route('/your-url', methods=['GET', 'POST'])
 def your_url():
@@ -30,7 +28,7 @@ def your_url():
         else:
             f = request.files['file']
             full_name = request.form['code'] + secure_filename(f.filename)
-            f.save('/home/marcon/Documents/tutorials/url-shortner/' + full_name)
+            f.save('/home/marcon/Documents/tutorials/url-shortner/static/user_files' + full_name)
             urls[request.form['code']] = {'file': full_name}
 
         with open('urls.json', 'w') as url_file:
@@ -40,5 +38,20 @@ def your_url():
     else:
         return redirect(url_for('home'))
 
+@app.route('/<string:code>')
+def redirect_to_url(code):
+    if os.path.exists('urls.json'):
+        with open('urls.json') as urls_file:
+            urls = json.load(urls_file)
+            if code in urls.keys():
+                if 'url' in urls[code].keys():
+                    return redirect(urls[code]['url'])
+                else:
+                    return redirect(url_for('static',filename='user_files/'+urls[code]['file']))
+    return abort(404)
 
-@app.route('/')
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('page_not_found.html'), 404
+
+
